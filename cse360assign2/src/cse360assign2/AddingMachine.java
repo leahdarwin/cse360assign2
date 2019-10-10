97efeda878c475ae9a848821e80608fcaedd0c7a
Leah Darwin <leahdarwin1@gmail.com>
2019-10-09 20:14:46 -0700

    Final commit, assignment completed.

--- cse360assign2/src/cse360assign2/AddingMachine.java
+++ cse360assign2/src/cse360assign2/AddingMachine.java
@@ -3,21 +3,24 @@ package cse360assign2;
 public class AddingMachine {
 
 	private int total;
+	private String record = "0";
 	
 	public AddingMachine () {
 		total = 0;  // not needed - included for clarity
 	}
 	
 	public int getTotal () {
-		return 0;
+		return total;
 	}
 	
 	public void add (int value) {
-		
+		total = value + total;
+		record = record + " + " + Integer.toString(value);
 	}
 	
 	public void subtract (int value) {
-		
+		total = total - value;
+		record = record + " - " + Integer.toString(value);
 	}
 		
 	public String toString () {
@@ -25,6 +28,7 @@ public class AddingMachine {
 	}
 
 	public void clear() {
-	
+		total = 0;
+		record = "0";
 	}
 }
